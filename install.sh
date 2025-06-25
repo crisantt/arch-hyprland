1ebdc5ba2d519c3e6ec5c4bd2d0c0b503dfa45b7
Cris <152991141+crisantt@users.noreply.github.com>
2025-06-25 19:46:46 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Dependencies
+sudo apt-get install -y \
+        build-essential \
+        git \
+        libx11-dev \
+        libx11-xcb-dev \
+        libxcb-res0-dev \
+        libxinerama-dev \
+        libxft-dev \
+        libimlib2-dev \
+
+# Core Packages
+sudo apt-get install -y \
+        xorg \
+        xorg-dev \
+        xbacklight \
+        xbindkeys \
+        xvkbd \
+        xinput \
+        xclip \
+        build-essential \
+        xdotool \
+        libnotify-bin \
+        libnotify-dev \
+
+# UI Packages
+sudo apt-get install -y \
+        rofi \
+        dunst \
+        feh \
+        lxappearance \
+        network-manager-gnome \
+        xdg-user-dirs-gtk \
+
+# Audio Packages
+ sudo apt-get install -y \
+        pavucontrol \
+        pulsemixer \
+        pamixer \
+        pipewire-audio \
+
+# Font Packages
+sudo apt-get install -y \
+        fonts-recommended \
+        fonts-terminus \
+        fonts-dejavu \
+        fonts-font-awesome \
+        fonts-noto-core \
+        fonts-noto-cjk \
+        fonts-noto-color-emoji \
+        fonts-hack-ttf \
