97e180225a8b1625dff00a098cdf11206e4f8b19
Cris <152991141+crisantt@users.noreply.github.com>
2025-07-01 17:21:03 +0800

    Update install-new.sh

--- install-new.sh
+++ install-new.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
-sudo apt install -y libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev cmake libxft-dev libimlib2-dev libxinerama-dev libxcb-res0-dev alsa-utils
+sudo apt install -y libconfig-dev \
+libdbus-1-dev \
+libegl-dev \
+libev-dev \
+libgl-dev \
+libepoxy-dev \
+libpcre2-dev \
+libpixman-1-dev \
+libx11-xcb-dev \
+libxcb1-dev \
+libxcb-composite0-dev \
+libxcb-damage0-dev \
+libxcb-dpms0-dev \
+libxcb-glx0-dev \
+libxcb-image0-dev \
+libxcb-present-dev \
+libxcb-randr0-dev \
+libxcb-render0-dev \
+libxcb-render-util0-dev \
+libxcb-shape0-dev \
+libxcb-util-dev \
+libxcb-xfixes0-dev \
+libxext-dev \
+meson \
+ninja-build \
+uthash-dev \
+cmake \
+libxft-dev \
+libimlib2-dev \
+libxinerama-dev \
+libxcb-res0-dev \
+alsa-utils \
