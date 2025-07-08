3bbff9b57b05feb81490f473afc535c41dfc455e
Cris <152991141+crisantt@users.noreply.github.com>
2025-07-09 00:47:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,59 +1,10 @@
 #!/bin/bash
 
-# Dependencies
-sudo apt-get install -y \
-        build-essential \
-        git \
-        libx11-dev \
-        libx11-xcb-dev \
-        libxcb-res0-dev \
-        libxinerama-dev \
-        libxft-dev \
-        libimlib2-dev \
+#packages
+sudo pacman -S smartmontools polkit-kde-agent qt5-wayland qt6-wayland uwsm wget wireless_tools wofi xdg-desktop-portal-hyprland xdg-utils openssh
 
-# Core Packages
-sudo apt-get install -y \
-        xorg \
-        xinit \
-        x11-xserver-utils \
-        xserver-xorg-video-amdgpu \
-        xorg-dev \
-        xbacklight \
-        xbindkeys \
-        xvkbd \
-        xinput \
-        xclip \
-        xdotool \
-        libnotify-bin \
-        libnotify-dev \
-        clangd \
-        make \
-        curl \
-        wget \
+# wireless tools
+# sudo pacman -S wpa_supplicant iwd
 
-# UI Packages
-sudo apt-get install -y \
-        rofi \
-        dunst \
-        feh \
-        lxappearance \
-        network-manager-gnome \
-        xdg-user-dirs-gtk \
-
-# Audio Packages
- sudo apt-get install -y \
-        pavucontrol \
-        pulsemixer \
-        pamixer \
-        pipewire-audio \
-
-# Font Packages
-sudo apt-get install -y \
-        fonts-recommended \
-        fonts-terminus \
-        fonts-dejavu \
-        fonts-font-awesome \
-        fonts-noto-core \
-        fonts-noto-cjk \
-        fonts-noto-color-emoji \
-        fonts-hack-ttf \
+#user packages
+sudo pacman -S dunst dolphin kitty hyprland htop
